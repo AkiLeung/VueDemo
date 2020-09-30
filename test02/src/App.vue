c29a8db244a42475b4fc02310f5d4831bc7b80a3
AkiLeungQ <382926924@qq.com>
2020-09-29 19:27:39 -0700

    update 20200929

--- test02/src/App.vue
+++ test02/src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
+    <div>
+      <el-row>
+        <el-button>默认按钮</el-button>
+        <el-button type="primary">主要按钮</el-button>
+        <el-button type="success">成功按钮</el-button>
+        <el-button type="info">信息按钮</el-button>
+        <el-button type="warning">警告按钮</el-button>
+        <el-button type="danger">危险按钮</el-button>
+      </el-row>
+    </div>
   </div>
 </template>
 
@@ -12,7 +22,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- test02/src/router/index.js
+++ test02/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Test01 from '@/components/Test01'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUi)
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Test01',
-      component: Test01
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Test01',
+    component: Test01
+  }]
 })
