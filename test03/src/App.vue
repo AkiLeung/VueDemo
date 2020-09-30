d4bd4592f4af74eba01d51fed11ce0492a96c9b8
AkiLeungQ <382926924@qq.com>
2020-09-29 22:39:35 -0700

    update 20200929

---
+++ test03/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h2>Essential Links</h2>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="text">文字按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
