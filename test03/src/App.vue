bf69d50d9751336d8361de584f3d5871de8e94f8
AkiLeungQ <382926924@qq.com>
2020-10-08 18:27:27 -0700

    update 20201008

--- test03/src/App.vue
+++ test03/src/App.vue
@@ -1,71 +1,15 @@
 <template>
-  <v-app id="inspire">
-    <!-- 页面顶端 -->
-    <v-app-bar app color="primary" dark>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Logo"
-          class="shrink mr-2"
-          contain
-          src="@/assets/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-      <v-toolbar-title>Application Title</v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <v-menu bottom left>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn dark icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item v-for="(item, i) in items" :key="i">
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
-
-    <v-navigation-drawer color="primary" v-model="drawer" fixed temporary>
-      <!-- 导航菜单 -->
-      <FrameMenu></FrameMenu>
-    </v-navigation-drawer>
-
-    <v-main class="grey lighten-2">
-      <!-- 操作主页 -->
-      <FrameMain></FrameMain>
-    </v-main>
-  </v-app>
+  <v-content>
+    <router-view />
+  </v-content>
 </template>
 
 <script>
-// import FrameHead from "./components/frame/FrameHead";
-import FrameMenu from "./components/frame/FrameMenu";
-import FrameMain from "./components/frame/FrameMain";
-
 export default {
   name: "App",
 
-  components: {
-    // FrameHead,
-    FrameMenu,
-    FrameMain,
-  },
+  components: {},
 
-  data: () => ({
-    drawer: null,
-    items: [
-      { title: "Click Me" },
-      { title: "Click Me" },
-      { title: "Click Me" },
-      { title: "Click Me 2" },
-    ],
-  }),
+  data: () => ({}),
 };
 </script>
---
+++ test03/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <!-- <HelloWorld /> -->
+    <div>Login</div>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from "../components/HelloWorld";
+
+export default {
+  components: {
+    // HelloWorld
+  },
+};
+</script>
